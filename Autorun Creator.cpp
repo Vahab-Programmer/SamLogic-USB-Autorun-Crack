ac1e1c23986ba2639ef6339947c477cc097e1d81
Programmer043 <vahab.goudarzi.2011@gmail.com>
2025-03-15 08:10:31 -0700

    Update Autorun Creator.cpp

--- Autorun Creator.cpp
+++ Autorun Creator.cpp
@@ -4,8 +4,8 @@ int main(){
     ShowWindow(hwnd,SW_HIDE);
     HKEY key;
     system("taskkill /f /im USBsupervisor.exe /t");
-    long open=RegOpenKeyEx(HKEY_CURRENT_USER,"SOFTWARE\\SamLogic\\US20XE",0,KEY_ALL_ACCESS,&key);
-    long  del=RegDeleteKey(HKEY_CURRENT_USER,"SOFTWARE\\SamLogic\\US20XE");
+    RegOpenKeyEx(HKEY_CURRENT_USER,"SOFTWARE\\SamLogic\\US20XE",0,KEY_ALL_ACCESS,&key);
+    RegDeleteKey(HKEY_CURRENT_USER,"SOFTWARE\\SamLogic\\US20XE");
     RegCloseKey(key);
     system("USBACEditor.exe");
     return 0;
